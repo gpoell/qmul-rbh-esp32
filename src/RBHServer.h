c17bbc4783e414dd1711954c3cff61a8168f0cd6
Garrett Poell <gpoell.gp@gmail.com>
2024-05-23 15:20:21 +0100

    creating initial class to abstract  WiFi and Server functionality (#6)

---
+++ src/RBHServer.h
@@ -0,0 +1,16 @@
+#ifndef RBHServer_h
+#define RBHServer_h
+#include "WiFi.h"
+
+class RBHServer {
+    private:
+
+    public:
+        WiFiServer server;
+        WiFiClient client;
+        void init(String ssid, String passwd);
+        void set_ipaddress();
+        void start_server();
+};
+
+#endif
