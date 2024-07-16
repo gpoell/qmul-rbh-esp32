22bc8950a4ec1b29d216f89ae6d1579b954e5f8a
Garrett Poell <gpoell.gp@gmail.com>
2024-07-16 19:03:46 +0100

    Feature - collection method (#18)
    * decoupling stopping gripper and motor delay with open and close commands
    
    * expanding process_command functionality and including new collection method
    
    * adding persistent gui client parameter to new send method
    
    * updating class descriptions

--- include/ESPServer.h
+++ include/ESPServer.h
@@ -6,21 +6,24 @@
  *    
  *      server <WiFiServer>:     creates a server that listens for incoming connections on
  *                               a specified port.
- *      guiClient <WiFiClient>:  persists the client for sending tactile data
+ *      guiClient <WiFiClient>:  persists the client for continuously sending tactile data
+ *                               to the GUI and unblocking incoming GUI commands.
  *      prompt <CommandPrompt>:  displays important information to the user through the
  *                               Serial Monitor.
  *      sensor <TactileSensor>:  collects magnetic flux density recordings from the
  *                               Hall Effect sensors
- *      motor <L9110HMotor>:     opens and closes the gripper
- *      data <vector3Double>:    structure of integers for capturing sensor recordings
- *      connected <bool>:        indicator for sending tactile data to GUI
+ *      motor <L9110HMotor>:     performs motor operations
+ *      connected <bool>:        flag for continuously sending tactile data to GUI
  * 
  * Methods:
  * 
- *     process_command:          executes gripper functionality based on commands
- *     is_connected:             returns the connection status for reading tactile data
- *     get_tactile_data:         sends tactile data recordings to the GUI
+ *     init:                     initializes sensors and communication protocols
+ *     process_command:          executes gripper functionality based on GUI commands
+ *     send_tactile_data:        sends tactile data to GUI with standardized buffer message
  *     create_buffer_message:    resizes the message to match the GUI buffer request
+ *     is_connected:             returns the connection status for reading tactile data
+ *     get_server:               returns client connection from GUI
+ *     get_client:               returns the stored client for continuously sending data to GUI
  * 
  */
 
@@ -35,6 +38,7 @@
 #include "TactileSensor.h"
 #include "L9110HMotor.h"
 
+using namespace std;
 
 class ESPServer {
     private:
@@ -43,18 +47,18 @@ class ESPServer {
         CommandPrompt prompt;
         TactileSensor sensor{1};
         L9110HMotor motor;
-        vector3Double data;
         bool connected {false};
 
     public:
         ESPServer(const int port) : server(port) {};
         ~ESPServer() {};
         void init();
-        WiFiClient client_available();
         void process_command(const String& cmd, WiFiClient& client);
-        bool is_connected() {return connected;};
-        void get_tactile_data();
-        const char* create_buffer_message(std::string& message, int size);
+        void send_tactile_data(WiFiClient& client);
+        const char* create_buffer_message(string& message, int size);
+        bool is_connected() { return connected; };
+        WiFiClient get_server() { return server.available(); };
+        WiFiClient get_client() { return guiClient; };
 };
 
 #endif
